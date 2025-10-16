2c4e9a1a5385db143d83b01d1ee5d2b29b8b351f
DIMITRIS LAMPROU <dimitris2001lamprou@gmail.com>
2025-10-16 17:22:27 +0300

    Add project files.

---
+++ ClientData/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ClientData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(306, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(324, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(926, 595);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ClientData/Form1.cs
@@ -0,0 +1,211 @@
+﻿using Ionic.Zip;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ClientData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Unzip();
+            RunPackageSSIS();
+            LoadClients();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Unzip()
+        {
+            string zipPath = Path.Combine(Application.StartupPath, "Data/Files.zip");
+            string extractPath = Path.Combine(Application.StartupPath, "ExtractedFiles");
+
+            try
+            {
+                if (!Directory.Exists(extractPath))
+                    Directory.CreateDirectory(extractPath);
+
+                using (ZipFile zip = ZipFile.Read(zipPath))
+                {
+                    zip.Password = "FirstCall13";
+                    zip.ExtractAll(extractPath, ExtractExistingFileAction.OverwriteSilently);
+                }
+
+                MessageBox.Show("Το unzip ολοκληρώθηκε επιτυχώς!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Σφάλμα κατά το unzip: " + ex.Message);
+            }
+        }
+
+        private void RunPackageSSIS()
+        {
+            string ssisPackagePath = Path.Combine(Application.StartupPath, @"SSIS Package\Package.dtsx");
+            string[] possibleDTexecPaths = {
+                                            @"C:\Program Files\Microsoft SQL Server\150\DTS\Binn\dtexec.exe",
+                                            @"C:\Program Files\Microsoft SQL Server\160\DTS\Binn\dtexec.exe",
+                                            @"C:\Program Files (x86)\Microsoft SQL Server\150\DTS\Binn\dtexec.exe",
+                                            @"C:\Program Files\Microsoft SQL Server\140\DTS\Binn\dtexec.exe"
+            };
+
+            string dtexecPath = possibleDTexecPaths.FirstOrDefault(File.Exists);
+            if (dtexecPath == null) throw new FileNotFoundException("Cannot find dtexec.exe");
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = dtexecPath;
+            psi.Arguments = $@"/FILE ""{ssisPackagePath}"" /CHECKPOINTING OFF";
+
+            psi.CreateNoWindow = false;
+            psi.UseShellExecute = false;
+
+            Process proc = new Process();
+            proc.StartInfo = psi;
+            proc.Start();
+
+            proc.WaitForExit();
+
+            if (proc.ExitCode == 0)
+            {
+                // Εδώ η εκτέλεση ήταν επιτυχής
+                MessageBox.Show("Το SSIS package ολοκληρώθηκε επιτυχώς!");
+            }
+            else
+            {
+                // Κάποιο σφάλμα συνέβη
+                MessageBox.Show("Σφάλμα κατά την εκτέλεση του SSIS package");
+            }
+        }
+        private void LoadClients()
+        {
+            string connectionString = @"Data Source=LAPTOP-K0MBAPHV\SQLSERVERDEVED;Initial Catalog=FirstCall Project;Integrated Security=True;";
+            string query = "SELECT DISTINCT lastName FROM Client ORDER BY lastName";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        comboBox1.Items.Clear();
+                        while (reader.Read())
+                        {
+                            comboBox1.Items.Add(reader["lastName"].ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Σφάλμα κατά τη φόρτωση πελατών: " + ex.Message);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+
+            string selectedLastName = comboBox1.SelectedItem.ToString();
+
+            string connectionString = @"Data Source=LAPTOP-K0MBAPHV\SQLSERVERDEVED;Initial Catalog=FirstCall Project;Integrated Security=True;";
+
+            string query = @"
+        SELECT 
+            ISNULL(STRING_AGG(ph.phone, ', '), '') AS Phones,
+            p.paymentAmount,
+            cs.id AS CaseID,
+            cs.cardOrLoan,
+            cs.caseCode,
+            cs.acc,
+            cs.productCode,
+            cs.limitOrLoanAmount,
+            cs.balance,
+            cs.interestRate,
+            cs.overdueAmount,
+            cs.statementBalance,
+            cs.interest,
+            cs.assignmentDate,
+            cs.caseStatus,
+            cs.expiryDate,
+            cs.storeCode,
+            cs.assignmentCode,
+            cs.totalBalance,
+            cs.assignmentTotalBalance,
+            cs.assignmentEndlessAmount,
+            cs.assignmentOverdueAmount,
+            cs.availableBalance,
+            cs.revocationCode,
+            cs.caseBucket,
+            cs.daysLate,
+            cs.loanPurpose            
+        FROM Client c
+        LEFT JOIN Phone ph ON ph.clientID = c.id
+        LEFT JOIN [Relation] r ON r.clientID = c.id
+        LEFT JOIN [Case] cs ON cs.id = r.caseID
+        LEFT JOIN Payment p ON p.caseID = cs.id
+        WHERE c.lastName = @lastName
+        GROUP BY 
+            p.paymentAmount,
+            cs.id,
+            cs.cardOrLoan,
+            cs.caseCode,
+            cs.acc,
+            cs.productCode,
+            cs.limitOrLoanAmount,
+            cs.balance,
+            cs.interestRate,
+            cs.overdueAmount,
+            cs.statementBalance,
+            cs.interest,
+            cs.assignmentDate,
+            cs.caseStatus,
+            cs.expiryDate,
+            cs.storeCode,
+            cs.assignmentCode,
+            cs.totalBalance,
+            cs.assignmentTotalBalance,
+            cs.assignmentEndlessAmount,
+            cs.assignmentOverdueAmount,
+            cs.availableBalance,
+            cs.revocationCode,
+            cs.caseBucket,
+            cs.daysLate,
+            cs.loanPurpose";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@lastName", selectedLastName);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Σφάλμα κατά τη φόρτωση δεδομένων: " + ex.Message);
+            }
+        }
+    }
+}
